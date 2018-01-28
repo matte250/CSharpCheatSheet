39eba42b6e7bc454cc81bd5c1d67a4b3dcb4edd7
matte250 <matte_1500@hotmail.com>
2018-01-28 13:20:50 +0100

    Iteration 28012018

---
+++ Iteration/Iteration/Program.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,z, sum;
+            String str;
+            // (Exercise 20) Ask the user to enter a value. If the value is greater than zero, then iterate value x amount of times.
+            // Print each iteration.
+            
+            PrintExercise(20, "Simple iteration.");
+
+            Console.Write("Enter value: ");
+            x = Int32.Parse(Console.ReadLine());
+            if (x > 0)
+            {
+                for(int i = 0; i < x; i++)
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 21) Ask the user to enter a value. If the value is greater than zero, then iterate value x amount of times bakwards.
+            // Print each iteration.
+
+            PrintExercise(21, "Simple backwards iteration.");
+
+            Console.Write("Enter value: ");
+            x = Int32.Parse(Console.ReadLine());
+            if (x > 0)
+            {
+                for (int i = x-1; i >= 0; i--)
+                    Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 22) Ask the user to enter a value. If the value is greater than zero, then interate value x amount of times.
+            // Print each iteration divisible by 2.
+
+            PrintExercise(22, "Modulus(%2) iteration.");
+
+            Console.Write("Enter value: ");
+            x = Int32.Parse(Console.ReadLine());
+            if (x > 0)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    if(i%2==0)
+                    Console.WriteLine(i);
+                }
+                  
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 23) Print out a multiplication table using iteration.
+
+            PrintExercise(23, "Multiplication table.");
+            
+            for(int y = 1; y < 11; y++)
+            {
+                for(int i = 1; i < 11; i++)
+                {
+                    if (i * y < 10) {
+                        Console.Write("  ");
+                    } else {
+                        Console.Write(" ");
+                    }
+                    Console.Write(i*y);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 24) Ask the user to enter a value. If the value is greater than zero, then iterate value x amount of times.
+            // Print each iteration to the power of 2 and its caluation.
+
+            PrintExercise(24, "Math.Pow iteration.");
+
+            Console.Write("Enter value: ");
+            x = Int32.Parse(Console.ReadLine());
+            if (x > 0)
+            {
+                for (int i = 0; i < x; i++)
+                    Console.WriteLine(i + "raised to the power of 2 is equal to " + Math.Pow(i,2));
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 25) Output dashes to the console that iterate backwards.
+
+            PrintExercise(25, "String manipulation iteration.");
+
+            str = String.Concat(Enumerable.Repeat("- ", 5));
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(str);
+                str = str.Substring(2);
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 26) Keep asking the user to input values into the user inputs the value -1. When the user inputs -1
+            // display the sum and average of all the values the user has entered.
+
+            PrintExercise(26, "Variable iteration.");
+
+            int total = 0, lastValue = 0, iterations = 0;
+            Console.Write("Enter value: ");
+            lastValue = Int32.Parse(Console.ReadLine());
+            while (lastValue != -1){
+                iterations += 1;
+                total += lastValue;
+                Console.Write("Enter value: ");
+                lastValue = Int32.Parse(Console.ReadLine());
+            }
+
+
+             
+            if(iterations != 0)
+            {
+                Console.WriteLine("Sum: {0}.\n" +
+                    "Average: {1}", total, (double)total/iterations );
+            }
+            else
+            {
+                Console.WriteLine("You didnt enter any values.");
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 27) Ask the user to input a value, then display all the values that the input can be divisible by.
+
+            PrintExercise(27, "Divisible iteration.");
+
+            Console.Write("Input value: ");
+            x = Int32.Parse(Console.ReadLine());
+            for(int i = 1; i < x; i++)
+            {
+                if (x % i == 0) Console.WriteLine(i);
+            }
+
+            Console.ReadKey();            
+
+            // (Exercise 28) Use iteration to print out atleast the first three perfect numbers.
+
+            PrintExercise(28, "Perfect number iteration.");
+
+            sum = 0;
+            Console.Write("Enter iteration value: ");
+            x = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < x; i++)
+            {
+                sum = 0;
+                for (int n = 1; n < i; n++)
+                {
+                    if (i % n == 0)
+                    {
+                        sum = sum + n;
+                    }
+                }
+                if (sum == i)
+                {
+                    Console.WriteLine(sum);
+                }
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 29) Ask the user to input a value. Then use this to iterate Fibonacci value x amount of times.
+
+            PrintExercise(29, "Fibonacci iteration.");
+
+            int a = 0, b = 1, temp = 0;
+            Console.Write("Enter value: ");
+            x = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("0\n1");
+
+            for (int i = 0; i < x; i++)
+            {
+                temp = a + b;
+                Console.WriteLine(temp);
+                a = b;
+                b = temp;
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 30) Ask the user to input a value, then iterate dashes to the console that goes back and forth.
+
+            PrintExercise(30, "Double string iteration.");
+            Console.Write("Enter value: ");
+            x = Int32.Parse(Console.ReadLine());
+            str = String.Concat(Enumerable.Repeat("- ", x));
+
+            for (int i = x; i >= 1; i--)
+            {
+                Console.WriteLine(String.Concat(Enumerable.Repeat("- ", i)));
+            }
+
+            for (int i = 1; i < x+1; i++)
+            {
+                Console.WriteLine(String.Concat(Enumerable.Repeat("- ", i)));
+            }
+
+            Console.ReadKey();
+
+            
+
+            // (Exercise 31) Let the user input a string, then check if that string is a palindrome.
+
+            PrintExercise(31, "Palindrome iteration");
+
+            String tempStr = "";
+
+            Console.Write("Write a word or sentence: ");
+            str = Console.ReadLine().ToLower().Replace(" ","");
+
+            for(int i = str.Length-1; i >= 0; i--)
+            {
+                tempStr += str[i];
+            }
+
+            if (str.Equals(tempStr))
+            {
+                Console.WriteLine("Your input is a palindrome! :D");
+            }
+            else
+            {
+                Console.WriteLine("Your input isnt a palindrome. :C");
+            }
+
+            Console.ReadKey();
+
+            // (Exercise 32) Make a menu system using a while loop. The menu should have 4 different options, and 3 of those
+            // options should lead back to the menu.
+
+            bool menu = true;
+            while (menu)
+            {
+                PrintExercise(32, "While-Loop menu.");
+
+                Console.WriteLine("MENU\n" +
+                    "1. Addition\n" +
+                    "2. String multiplication\n" +
+                    "3. Uppercase string\n" +
+                    "4. Exit");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        PrintExercise(32, "While-Loop menu.");
+                        Console.WriteLine("ADDITION");
+                        Console.Write("Enter value A: ");
+                        x = Int32.Parse(Console.ReadLine());
+                        Console.Write("Enter value B: ");
+                        z = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Result: {0}",x+z);
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        PrintExercise(32, "While-Loop menu.");
+                        Console.WriteLine("STRING MULTIPLICATION");
+                        Console.Write("Enter string: ");
+                        str = Console.ReadLine();
+                        Console.Write("Enter value: ");
+                        x = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("Result: " + String.Concat(Enumerable.Repeat(str, x)));
+                        Console.ReadKey();
+                        break;
+                    case "3":
+                        PrintExercise(32, "While-Loop menu.");
+                        Console.WriteLine("UPPERCASE STRING");
+                        Console.Write("Enter string: ");
+                        str = Console.ReadLine();
+                        Console.WriteLine("Result: " + str.ToUpper());
+                        Console.ReadKey();
+                        break;
+                    case "4":
+                        menu = false;
+                        break;
+                    default:
+                        PrintExercise(32, "While-Loop menu.");
+                        Console.WriteLine("Invalid input.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+
+
+        }
+
+        public static void PrintExercise(int x, string desc)
+        {
+            // Printing format for exercise descriptions.
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("(Exercise {0}) ", x);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(desc);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(String.Concat(Enumerable.Repeat("- ", 20)));
+        }
+    }
+}
--- Selection/Selection/Program.cs
+++ Selection/Selection/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +10,9 @@ namespace Selection
     {
         static void Main(string[] args)
         {
-            String str;
+            String str, strTemp = "";
             int temp;
-
+            
             // (Exercise 12) Ask the user to input a name. If the name is Bob or Alice, then display "Hi Bob!" or "Hi Alice!".
 
             Console.Write("Enter name: ");
@@ -20,18 +20,189 @@ namespace Selection
             if (str.Contains("bob")) Console.WriteLine("Hi Bob!");
             if (str.Contains("alice")) Console.WriteLine("Hi Alice!");
 
-            // (Exercise 13) Ask the user to input a number. if that number is divesible by 4, then display the result.
+            // (Exercise 13) Ask the user to input a number. If that number is divesible by 4, then display the result.
 
             Console.Write("Enter a number divisible by 4: ");
             temp = Int32.Parse(Console.ReadLine());
             if (temp % 4 == 0)
             {
-                Console.WriteLine(temp/4);
+                Console.WriteLine(temp / 4);
             }
             else
             {
                 Console.WriteLine("Your number isnt divisble by 4.");
             }
+
+            // (Exercise 14) Ask the user to input some text. If that text includes the word "city", then display at what position the word
+            // occures in the text.
+
+            temp = 0;
+            Console.Write("Write any sentence: ");
+            str = Console.ReadLine().ToLower();
+            
+            if (str.Contains("city"))
+            {
+                while (str.Contains("city"))
+                {
+                    strTemp += str.IndexOf("city") + ",";             
+                    str = str.Substring(0,str.IndexOf("city")+4).Replace("city","null") + str.Substring(str.IndexOf("city") + 4);
+                }
+
+                strTemp = strTemp.Remove(strTemp.LastIndexOf(","));
+
+                if (strTemp.Contains(","))
+                {
+                    Console.WriteLine("The word city occurces at positions " + strTemp + ".");
+                }
+                else
+                {
+                    Console.WriteLine("The word city occures at position " + strTemp + ".");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your sentence has no occurences of the word \"city\".");
+            }
+       
+
+            // (Exercise 15) Ask the user to input some text. Check if the length of the text excedes 25 characters.
+            // Display result.
+
+            Console.Write("Input text: ");
+            str = Console.ReadLine();
+            if (str.Length > 25)
+                Console.WriteLine("Your input excedes 25 characters.");
+            else
+                Console.WriteLine("Your input does not excede 25 characters.");
+           
+
+            // (Exercise 16) Display three different options for the user to choose from. Clear the console then display a different
+            // message deepending on the option. Display also a message if the choice is invalid.
+
+            Console.WriteLine("Input one of these following options.\n" +
+                "1. Option 1\n" +
+                "2. Option 2\n" +
+                "3. Option 3");
+            Console.Write("Input numerical: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine("This is option 1! :)");
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine("This is option 2! :3");
+                    break;
+                case "3":
+                    Console.Clear();
+                    Console.WriteLine("This is option 3! :D");
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Invalid option! :C");
+                    break;
+
+            }
+            
+
+            // (Exercise 17) Ask the user to input two intergers A and B. Then show results of A+B, A*B, A/B.
+            // Dont let user divide by zero.
+
+            Console.Write("Input value A: ");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.Write("Input value B: ");
+            int b = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Results\n" +
+                "A + B = " + (a+b) + "\n" +
+                "A * B = " + (a*b));
+            if (b != 0)
+            {
+                Console.WriteLine("A / B = " + ((double)a/b));
+            }
+            else
+            {
+                Console.WriteLine("A / B = N/A(Cant divide by 0)");
+            }
+
+            
+
+            // (Exercise 18) Ask the user to input a number, then check if the number is higher or equal than 10 and less or equal than 20.
+            // Display the result.
+
+            Console.Write("Input value: ");
+            temp = Int32.Parse(Console.ReadLine());
+            if (temp <= 20 && temp >= 10)
+            {
+                Console.WriteLine("Value is in range between 10 and 20." );
+            }else if(temp < 10)
+            {
+                Console.WriteLine("Value is less than 10.");
+            }
+            else
+            {
+                Console.WriteLine("Value is higher than 20.");
+            }
+            
+
+            // (Exercise 19) Ask the user to input a color, then do a switch statent to see if that color exists. If it does, change the
+            // console color and print a message of the color.
+
+            Console.Write("Write the name of a color: ");
+            str = Console.ReadLine();
+
+            switch (str.ToLower())
+            {
+                case "black":
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    break;
+                case "dark blue":
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    break;
+                case "dark green":
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case "dark red":
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case "dark magenta":
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    break;
+                case "dark yellow":
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case "dark gray":
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+                case "blue":
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "magenta":
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case "yellow":
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case "white":
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                default:
+                    Console.WriteLine("Invalid color name!");
+                    break;
+                   
+            }
+
+            Console.WriteLine(Console.ForegroundColor.ToString().ToUpper() + "!");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
 
         }
     }
