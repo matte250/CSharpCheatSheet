529ab9646c3ea161a22a8a6a15a8850c811f179a
Mattias Soroccos <matte_1500@hotmail.com>
2018-01-29 15:54:01 +0100

    20190129

---
+++ BlackListApplication/BlackListApplication/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackListApplication
+{
+    class Program
+    {
+
+        static string[] blackListArray;
+        static string[] messages;
+        static int messageNumber;
+
+        static void Main(string[] args)
+        {
+            /* (Exercise 42)
+             * The company See sharp AB have discovered that the users on their forums use a very harsh language
+             * when interacting with each other. So now they have asked you to write a swear word filter to censor these
+             * occurring words. Before implementing this filter on their website, they want a demonstration in form of a
+             * console program. 
+             */
+
+            blackListArray = new string[] {
+                "fuck","shit","ernes","hästar"
+            };
+            messages = new string[100];
+            messageNumber = 0;
+            string str = "";
+            while (!str.Equals("stop"))
+            {
+                Console.Clear();
+                Console.WriteLine("Type \"stop\" to quit.\n");
+
+                foreach(string s in messages)
+                {
+                    if (s != null) Console.WriteLine(s);
+                }
+
+                Console.Write("Input: ");
+                str = Console.ReadLine();
+                messages[messageNumber] = convertString(str);
+                messageNumber++;
+            }
+            
+
+        }
+
+        static string convertString(string str)
+        {
+            foreach(string s in blackListArray)
+            {
+                while (str.ToLower().Contains(s))
+                {
+                    int index = str.ToLower().IndexOf(s);
+                    str = str.Remove(index, s.Length);
+                    str = str.Insert(index, String.Concat(Enumerable.Repeat("*", s.Length)));
+                }
+            }
+            return str;
+        }
+    }
+}
---
+++ FruitsAndExceptions/FruitsAndExceptions/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitsAndExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* (Exercise 43)
+             * Given the string “[ { fruit:pear , colour:green }, { fruit:orange, colour: orange },{ fruit:apple, colour : red
+             * }]”, extract the fruits and their colour and display it to the user like: “The fruit pear is green”
+             */
+
+            int arrayLength = 0;
+            string[] fruitsAndColors = new string[100];
+            string str = "[ { fruit: pear , colour: green }, { fruit: orange, colour: orange },{" +
+                "fruit: apple, colour: red }]";
+            str = str.Replace("[ ", "");
+            str = str.Replace("]", "");
+            str = str.Replace(" ", "");
+            Console.WriteLine(str);
+            while (str.Contains("{"))
+            {
+                fruitsAndColors[arrayLength] = str.Substring(0, str.IndexOf("}")).Replace("{","");
+                str = str.Remove(0, 1);
+                if(str.Contains("{")) str = str.Remove(0, str.IndexOf("{"));
+                arrayLength++;
+            }
+
+            foreach(string s in fruitsAndColors)
+            {
+                if (s != null)
+                {
+                    string fruit = s.Remove(s.IndexOf(","));
+                    fruit = fruit.Substring(fruit.IndexOf(":")+1);
+                    Console.WriteLine(s);
+                    Console.WriteLine(fruit);
+                }
+            }
+                                           
+
+
+        }
+    }
+}
