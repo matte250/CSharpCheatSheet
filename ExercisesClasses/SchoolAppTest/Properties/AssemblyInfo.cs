e704b07fdeb33e363ae7e2707bb3ad06d841f68e
Mattias Soroccos <matte_1500@hotmail.com>
2018-02-09 15:59:48 +0100

    20180209school

---
+++ ExercisesClasses/SchoolAppTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SchoolAppTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SchoolAppTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("648c7fa5-8e62-47b9-89f3-5cd3c0ed08e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExercisesClasses/SchoolApplication/Classes/RandomNames.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class RandomNames
+{
+    public static Random r = new Random();
+    public static string[] randomProductNames = new string[]
+    {
+            "Condoms", "Sack of potatoes", "Dank memes", "Fish", "Mullet", "Placenta", "Illegal software", "Drugs",
+            "Air freshner", "Centaur", "Hat", "Husband pillow", "Girlfriend pillow", "Oni chan", "Afro", "Black tar heroin",
+            "Dildo", "Dragons", "Cube", "Kinder eggs", "Skittles", "Fried chicken", "Waifu", "Mexicans", "Calories"
+    };
+    public static string[] randomFirstNames = new string[]
+    {
+            "Bob","Mattias","Ernes","Philip","Filip","Memer","Lord","Mr","Kevin","Alpha","Jesus","Timmy","Eric","Adolf",
+            "Beth","Mathilda","Erin","Filicia","Filipia","Mavis","Pristess","Ms","Kathy","Zeta","Jasmine","Tina","Erika","Eva",
+            "Alex","Baily","Charlie","Devin","Eli","Frances","Glenn","Harley","Jayden","Kelly","Logan","Monroe","Pheonix","Quinn",
+            "Rowan","Sage","Taylor","Val","Winter","Salim"
+    };
+    public static string[] randomLastNames = new string[]
+    {
+            "Smith","Jones","Brown","Johnson","Williams","Miller","Taylor","Wilson","Davis","White","Clark","Hall","Thomas",
+            "Thompson","Moore","Hill","Walker","Andersson","Wright","Martin","Wood","Allen","Robinsson","Lewis","Scott","Knuckles",
+            "Young","Jacksson","Adams","Tryniski","Green","Evans","King","Baker","John","Harris","Robert","Campbell","James","Borgström",
+            "Soroccos","Philipowhich", "Filipovic"
+    };
+
+    public static string FirstName()
+    {
+        return randomFirstNames[r.Next(randomFirstNames.Length)];
+    }
+
+    public static string LastName()
+    {
+        return randomLastNames[r.Next(randomLastNames.Length)];
+    }
+
+    public static string ProductName()
+    {
+        return randomProductNames[r.Next(randomProductNames.Length)];
+    }
+
+}
