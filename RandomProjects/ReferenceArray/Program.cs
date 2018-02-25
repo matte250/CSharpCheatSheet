dc3ee6502471842ed35b3ca4903d159fcb656822
matte250 <matte_1500@hotmail.com>
2018-02-25 15:25:06 +0100

    Hejtest

---
+++ RandomProjects/ReferenceArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = new string[]{ "lol" };
+            Testing(a);
+            Console.WriteLine(a[0]);
+        }
+
+        static void Testing(string[] ab)
+        {
+            ab[0] = "memer";
+        }
+    }
+}
